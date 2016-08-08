a0d3d63ba1c4b2e8d1428e07b13a9709f4bc08e5
cgq5 <cgq55555@gmail.com>
2016-08-08 21:44:41 +0300

    add optimization

--- optimization.py
+++ optimization.py
@@ -22,13 +22,15 @@ def rahat():
     cons = ({'type': 'ineq', 'fun': lambda x:  150 - .1 *x[0] - .3 * x[1]},
     {'type': 'ineq', 'fun': lambda x: 51.65 - .05 * x[0] - .1 * x[1]},
     {'type': 'ineq', 'fun': lambda x: 14.99 - .03 * x[0] - .02 * x[1]})
-    bnds = ((0, None), (0, None))
+    bnds = [(0, None)] * 3
     res = minimize(fun, x0, method='SLSQP', bounds=bnds,
-    constraints=cons)
+        constraints=cons)
     print res.x
 
 def costFunc(quant):
-    cost = [105, 135, 153, 110, 140, 137, 130, 132, 115]
+    cost_keystone = [105, 135, 153, 110, 140, 137, 130, 132, 115]
+    cost_colombi = [15, 21, 17, 23.5, 25.5, 22]
+    cost = cost_colombi 
     total = sum([(x*y) for x,y in zip(cost, quant)])
     return total
 
@@ -51,10 +53,26 @@ def keystone():
     print x
     print("The minimum cost is %s" % (costFunc(x)))
 
+def colombi():
+    x0 = zeros(6)
+    # Objective function
+    cons = ({'type': 'ineq', 'fun': lambda x: x[0] + x[3] - 2000 },
+            {'type': 'ineq', 'fun': lambda x: x[1] + x[4] - 930 },
+            {'type': 'ineq', 'fun': lambda x: x[2] + x[5] - 2200 },
+            {'type': 'eq', 'fun': lambda x: sum(x[:3]) - 2500 },
+            {'type': 'eq', 'fun': lambda x: sum(x[3:6]) - 2630 })
+    bnds = [(0, None)] * 6 
+    res = minimize(costFunc, x0, method='SLSQP', bounds=bnds,
+        constraints=cons)
+    x = [ round(elem) for elem in res.x]
+    print x
+    print("The minimum cost is %s" % (costFunc(x)))
+
 def main():
-    # zooter()
-    # rahat()
-    keystone()
+    #zooter()
+    #rahat()
+    #keystone()
+    colombi()
 
 if __name__ == '__main__':
     main()
